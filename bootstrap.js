ebe489b9f81ee4816e2a9879dc28f0d22fc51b0d
Sapphire <sapphireyed@gmail.com>
2020-09-24 16:55:14 +0200

    new repo ql bootstrap

---
+++ bootstrap.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  // enebles bs4 tootips
+ $('[data-toggle="popover"]').popover();
+ // slide in features
+ $(document).on('scroll', function(){
+   let i = 0;
+   function slideFeature(){
+     let features = document.querySelectorAll(".features li");
+     if (i < features.length) {
+       features[i].classList.add('slideIn');
+       features[i].style.left = 0;
+       i++;
+     } else {
+       clearInterval(start)
+     }
+   }
+   let start = setInterval(slideFeature, 1200);
+ });
+ // popover responsive
+ $("#shoparea").on('click', function(){
+ var width = $("#map").width();
+ var height = $("#map").height();
+ var position = $("#map").offset();
+ $("#shop").offset({left:(position.left + (width * 0.27)),top:(position.top + (height * 0.33))});
+  $('#shop').trigger('click')
+ });
+ //choose hero
+ var heroes = document.getElementsByClassName('heroesdesc')
+ var avatars = document.getElementsByClassName('heroesav')
+   $(".heroesav").each(function(){
+     $(this).on('click', function(event){
+       $('#zoommap, #zoomstop').hide();
+       $('.main, .heroesdesc').hide();
+       $(this).find('.heroesdesc').show();
+       $(this).find('.heroesdesc').clone().appendTo('.exactdiv');
+       $(this).find('.heroesdesc').hide()
+  //     $(".exactdiv div:first-child").replaceWith( $(this).find('.heroesdesc') );
+     })
+   })
+   $(".map").click(function(){
+     $(".heroesdesc").hide();
+     $(".main").show()
+     $('#zoommap, #zoomstop').show();
+   })
+//zoom map
+  $('#zoommap').on('click', function(){
+    $('#ex1').zoom();
+  })
+ });
