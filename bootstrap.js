e3f90e62654def70590f18db68ffa7277693f021
Sapphire <sapphireyed@gmail.com>
2020-09-26 08:37:02 +0200

    first hero description added-knight. Fully responsive

--- bootstrap.js
+++ bootstrap.js
@@ -34,21 +34,67 @@ $(document).ready(function(){
  var avatars = document.getElementsByClassName('heroesav')
    $(".heroesav").each(function(){
      $(this).on('click', function(event){
-       $('#zoommap, #zoomstop').hide();
+       //hide zoom button, all heroes and main map
+       $('#zoommap').hide();
        $('.main, .heroesdesc').hide();
+       //show chosen hero in the div where main map was.
+       //Use clon to make the function work multiple times.
+       //Hide chosen hero in other places (the div from which it was cloned)
        $(this).find('.heroesdesc').show();
        $(this).find('.heroesdesc').clone().appendTo('.exactdiv');
        $(this).find('.heroesdesc').hide()
-  //     $(".exactdiv div:first-child").replaceWith( $(this).find('.heroesdesc') );
+       $('#prev, #next').show()
      })
    })
+   // choose hero menu to toggle on small screens
+   var x = window.matchMedia("(max-width: 760px)")
+   if ( x.matches) {
+     $('.heroesav').hide();
+     $(".map").hide();
+     $('.heroes h2:first-child').click(function(){
+       $('.heroesav').toggle();
+       $('.map').toggle();
+     })
+   }
+   // map button to come back to main map view.
    $(".map").click(function(){
      $(".heroesdesc").hide();
      $(".main").show()
-     $('#zoommap, #zoomstop').show();
+     $('#zoommap').show();
+     $('#prev, #next').hide()
    })
 //zoom map
   $('#zoommap').on('click', function(){
     $('#ex1').zoom();
   })
  });
+ //turn around hero
+ var i = 0;
+ let knightpositions = [
+   "img/knight1.png",
+   "img/knight2.png",
+   "img/knight3.png",
+   "img/knight5.png",
+   "img/knight.png"
+ ];
+
+ function turnLeftKnight() {
+   var knightel = document.getElementById('knight')
+   if (i < knightpositions.length-1) {
+     knightel.src = knightpositions[i+1];
+     i++;
+   } else {
+     knightel.src = knightpositions[0]
+     i=0;
+   }
+ }
+ function turnRightKnight() {
+   var knightel = document.getElementById('knight')
+   if (i == 0) {
+     knightel.src = knightpositions[knightpositions.length-1];
+     i = knightpositions.length-1;
+   } else if (i < knightpositions.length && i !== 0){
+     knightel.src = knightpositions[i-1]
+     i--;
+   }
+ }
